40094781809c0b97bc9a8d5616f536d2c45b23e0
Desmond Zhu <hz1778@nyu.edu>
2023-11-25 02:08:18 -0500

    feat: Add hero section to the homepage (#2)
    Implemented a new hero section in the homepage to provide a welcoming introduction and quick overview of the site's purpose. This section includes a main banner with a catchy tagline and a call-to-action button.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import HeroSection from './HeroSection';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <HeroSection></HeroSection>
     </div>
   );
 }
---
+++ src/HeroSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HeroSection() {
+  return (
+    <div className="hero-section">
+      <h1>Welcome to Dizzy!</h1>
+      <p>Your hub for creative and innovative projects.</p>
+    </div>
+  );
+}
+
+export default HeroSection;

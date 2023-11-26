7d7a505486dcb2824d2c094d47a38d2de8e38a59
Desmond Zhu <hz1778@nyu.edu>
2023-11-26 13:32:14 -0500

    feat(homepage): Implement dynamic and personalized layout (#9)
    * feat: Add hero section to the homepage
    
    Implemented a new hero section on the homepage to provide a welcoming introduction and quick overview of the site's purpose. This section includes a main banner with a catchy tagline and a call-to-action button.
    
    * feat: Integrate Firebase for backend services
    
    - Configured Firebase SDK with the React application.
    - Implemented Firebase Authentication for user sign-in and sign-up.
    - Set up Firestore Database for storing and retrieving project data.
    - Prepared Firebase Hosting for future deployment.
    
    This integration enables dynamic content management and user authentication, laying the groundwork for a fully functional application.

---
+++ src/HeaderBar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './HeaderBar.css'; 
+
+const HeaderBar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="header-bar">
+      <div className="brand-name">Dizzy</div>
+      
+      <div className="menu-container">
+        <button className="menu-toggle" onClick={toggleMenu}>
+            Menu
+        </button>
+        {isMenuOpen && (
+          <div className="dropdown-menu">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contact</a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HeaderBar;
--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import './HeroSection.css'; // Import the CSS file
+import myImage from "./DALL·E 2023-11-26 12.43.28 - A 3D animated character with a cheerful expression, waving hello. The character has an oval-shaped face, dark hair, glasses, and is dressed in a moder.png"
 
-function HeroSection() {
+const HeroSection = () => {
   return (
-    <div className="hero-section">
-      <h1>Welcome to Dizzy!</h1>
-      <p>Your hub for creative and innovative projects.</p>
+    <div className="hero-container">
+      <div className="hero-content">
+        <h2 className="hero-greeting">Hey! I'm Desmond 👋</h2>
+        <br></br>
+        <br></br>
+        <h1 className="hero-name-title">Full Stack Engineer</h1>
+        <h1 className="hero-name-title">& AI/ML Engineer</h1>
+        <h1 className="hero-name-title">@ <a href="http://www.caseguard.com" target="_blank" rel="noopener noreferrer" className="hero-company-link">CaseGuard</a></h1>
+        <br/>
+        <br></br>
+        <p className="hero-subtitle">Developing stuff since 2020</p>
+        <br/>
+        <br></br>
+        <br/>
+        <br></br>
+        <button className="hero-cta-button" onClick={() => { /* Scroll function or other action */ }}>Take a Look</button>
+      </div>
+      <div className="hero-image-container">
+        <img src={myImage} alt="Desmond waving" className="hero-image" />
+      </div>
     </div>
   );
-}
+};
 
 export default HeroSection;
